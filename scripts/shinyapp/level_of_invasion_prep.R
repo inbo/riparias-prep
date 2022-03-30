b1c1d0e7fed05d62ded9d2a4f0a120ae25436837
jrhillae <jasmijn.hillaert@inbo.be>
2022-03-30 11:36:51 +0200

    define level of invasion
    define level of invasion

--- scripts/shinyapp/level_of_invasion_prep.R
+++ scripts/shinyapp/level_of_invasion_prep.R
@@ -9,11 +9,31 @@ branch <- "41_extending_baseline_map"
 occupancy_RBU <- read.csv(paste0("https://github.com/inbo/riparias-prep/raw/", branch, "/data/interim/occupancy_RBU.csv"))
 occupancy_RBSU <- read.csv(paste0("https://github.com/inbo/riparias-prep/raw/", branch, "/data/interim/occupancy_RBSU.csv"))
 
-occupancy_RBSU <- occupancy_RBSU %>% select(-c(0,1))
+#remove first column
+occupancy_RBU <- occupancy_RBU %>% select(-c(1))
+occupancy_RBSU <- occupancy_RBSU %>% select(-c(1))
+ 
+#define level of invasion depending on relative occupancy
+occupancy_RBU_temp <- occupancy_RBU %>%
+  mutate(
+  level_of_invasion = case_when(
+    Occupancy_rel == 0 ~ "uninvaded",
+    Occupancy_rel <= 0.10 ~ "scattered occurences only",
+    Occupancy_rel <= 0.30 ~ "weakly invaded",
+    Occupancy_rel > 0.30 ~ "heavily invaded")
+  )
+   
+occupancy_RBSU_temp <- occupancy_RBSU %>%
+  mutate(
+    level_of_invasion = case_when(
+      Occupancy_rel == 0 ~ "uninvaded",
+      Occupancy_rel <= 0.10 ~ "scattered occurences only",
+      Occupancy_rel <= 0.30 ~ "weakly invaded",
+      Occupancy_rel > 0.30 ~ "heavily invaded")
+  )
 
-
-#save output
-write.csv(level_of_invasion_RBU, './data/interim/occupancy_RBU.csv')
-write.csv(level_of_invasion_RBSU, './data/interim/occupancy_RBSU.csv')
+#export output
+write.csv(occupancy_RBU_temp, './data/interim/occupancy_RBU.csv')
+write.csv(occupancy_RBSU_temp, './data/interim/occupancy_RBSU.csv')
 
 
