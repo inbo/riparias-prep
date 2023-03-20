cd36cdf7f32ec7c2475f7a614e13174ed0486f84
jrhillae <jasmijn.hillaert@inbo.be>
2023-03-20 16:37:15 +0100

    Update 8_baseline_maps_ui_D1.R

--- scripts/shinyapp/8_baseline_maps_ui_D1.R
+++ scripts/shinyapp/8_baseline_maps_ui_D1.R
@@ -12,7 +12,7 @@ library(trias)
 # Only packages installed from GitHub with devtools::install_github, in version 1.4 (or later) of devtools, are supported. Packages installed with an earlier version of devtools must be reinstalled with the later version before you can deploy your application. If you get an error such as “PackageSourceError” when you attempt to deploy, check that you have installed all the packages from Github with devtools 1.4 or later.
 
 #Reading in data####
-branch <- "50_add_species"
+branch <- "master"
 
 all_pointdata_2000 <- st_read("~/GitHub/riparias-prep/data/spatial/baseline/points_in_perimeter_sel.geojson")
 
